786442cc8bc4883c35ca30016e2353bef84ede01
Harshit Jain <90932614+harshitjain17@users.noreply.github.com>
2025-04-06 18:18:07 -0400

    Created the NewDeck() function

--- cards/deck.go
+++ cards/deck.go
@@ -5,8 +5,22 @@ import "fmt"
 //  Create a new type of 'deck' which is a slice of strings
 type deck []string
 
+// Create a new deck of cards with 52 cards
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+// Loop through the cards and print each one with its index
 func (d deck) print() {
-	// Loop through the cards and print each one with its index
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,9 @@
 package main
 
 func main() {
-	// Create a slice of cards and initialize with a few values
-	var cards = deck{newCard(), "Two of Spades", "Ace of Diamonds"}
+	// Create a new deck of cards
+	cards := newDeck()
 
-	// Append a new card to the slice
-	cards = append(cards, "Three of Hearts")
-
+	// Print the deck of cards
 	cards.print()
-}
-
-// newCard returns a predefined card as a string
-func newCard() string {
-	return "Five of Diamonds"
 }
