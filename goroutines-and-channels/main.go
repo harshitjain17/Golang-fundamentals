8fde1969e91dddb77b6090859e1ccaa78bfde60e
Harshit Jain <90932614+harshitjain17@users.noreply.github.com>
2025-04-14 00:15:12 -0400

    Goroutines implementation

--- goroutines-and-channels/main.go
+++ goroutines-and-channels/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
+// main function is the entry point of the program.
+// It initializes a list of URLs to be checked concurrently using goroutines and channels.
 func main() {
+	// List of websites to monitor.
 	links := []string{
 		"https://google.com",
 		"https://facebook.com",
@@ -14,18 +18,40 @@ func main() {
 		"https://amazon.com",
 	}
 
+	// Create a channel to receive status messages from goroutines.
+	c := make(chan string)
+
+	// Launch a goroutine for each link to check its status.
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	// Continuously listen for values from the channel.
+	// When a message is received, launch a new goroutine that waits 5 seconds before re-checking the link.
+	for l := range c {
+		// Capturing the value of 'l' to avoid closure pitfalls in goroutines.
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
-func checkLink(link string) {
+// checkLink performs a simple HTTP GET request to verify if the link is reachable.
+// It sends the link back through the channel regardless of the result,
+// allowing the link to be re-checked periodically.
+func checkLink(link string, c chan string) {
+	// Perform HTTP GET request
 	_, err := http.Get(link)
+
 	if err != nil {
+		// If there was an error (e.g., the site is down or unreachable), log it.
 		fmt.Println(link, "might be down!")
-		return
 	} else {
+		// If the GET request was successful, the link is up.
 		fmt.Println(link, "is up!")
-		return
 	}
+
+	// Send the link back into the channel for the next cycle.
+	c <- link
 }
